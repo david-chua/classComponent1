122c6d387c8abd472685c2169b3e108a07825d24
david-chua <dp_chua@hotmail.com>
2018-12-03 23:04:00 -0500

    completed class
    finished class

--- src/index.js
+++ src/index.js
@@ -63,167 +63,197 @@ const friendData = [
     id: 3,
     first_name: "Shelli",
     last_name: "Pywell"
-  },
-  {
-    id: 4,
-    first_name: "Westbrook",
-    last_name: "Jarrell"
-  },
-  {
-    id: 5,
-    first_name: "Deeanne",
-    last_name: "Daid"
-  },
-  {
-    id: 6,
-    first_name: "Dare",
-    last_name: "Chapelle"
-  },
-  {
-    id: 7,
-    first_name: "Elly",
-    last_name: "Belli"
-  },
-  {
-    id: 8,
-    first_name: "Josy",
-    last_name: "MacMychem"
-  },
-  {
-    id: 9,
-    first_name: "Xaviera",
-    last_name: "Ledgeway"
-  },
-  {
-    id: 10,
-    first_name: "Bryon",
-    last_name: "Zupo"
-  },
-  {
-    id: 11,
-    first_name: "Junette",
-    last_name: "Titheridge"
-  },
-  {
-    id: 12,
-    first_name: "Merrie",
-    last_name: "Pusey"
-  },
-  {
-    id: 13,
-    first_name: "Amelia",
-    last_name: "Stother"
-  },
-  {
-    id: 14,
-    first_name: "Rollo",
-    last_name: "Gamblin"
-  },
-  {
-    id: 15,
-    first_name: "Pavlov",
-    last_name: "Blunsom"
-  },
-  {
-    id: 16,
-    first_name: "Raddie",
-    last_name: "Cockerill"
-  },
-  {
-    id: 17,
-    first_name: "Bobbie",
-    last_name: "Randales"
-  },
-  {
-    id: 18,
-    first_name: "Dorette",
-    last_name: "Duffet"
-  },
-  {
-    id: 19,
-    first_name: "Tanitansy",
-    last_name: "A'field"
-  },
-  {
-    id: 20,
-    first_name: "Elvera",
-    last_name: "Anthoin"
-  },
-  {
-    id: 21,
-    first_name: "Ginger",
-    last_name: "Kopf"
-  },
-  {
-    id: 22,
-    first_name: "Aila",
-    last_name: "Killcross"
-  },
-  {
-    id: 23,
-    first_name: "Mycah",
-    last_name: "Paris"
-  },
-  {
-    id: 24,
-    first_name: "Patti",
-    last_name: "Quickenden"
-  },
-  {
-    id: 25,
-    first_name: "Cirillo",
-    last_name: "Rollings"
-  },
-  {
-    id: 26,
-    first_name: "Alameda",
-    last_name: "Kitcher"
-  },
-  {
-    id: 27,
-    first_name: "Mitzi",
-    last_name: "Pullin"
-  },
-  {
-    id: 28,
-    first_name: "Davide",
-    last_name: "Joust"
-  },
-  {
-    id: 29,
-    first_name: "Olenolin",
-    last_name: "Lean"
-  },
-  {
-    id: 30,
-    first_name: "Clarance",
-    last_name: "Clear"
   }
+  // {
+  //   id: 4,
+  //   first_name: "Westbrook",
+  //   last_name: "Jarrell"
+  // },
+  // {
+  //   id: 5,
+  //   first_name: "Deeanne",
+  //   last_name: "Daid"
+  // },
+  // {
+  //   id: 6,
+  //   first_name: "Dare",
+  //   last_name: "Chapelle"
+  // },
+  // {
+  //   id: 7,
+  //   first_name: "Elly",
+  //   last_name: "Belli"
+  // },
+  // {
+  //   id: 8,
+  //   first_name: "Josy",
+  //   last_name: "MacMychem"
+  // },
+  // {
+  //   id: 9,
+  //   first_name: "Xaviera",
+  //   last_name: "Ledgeway"
+  // },
+  // {
+  //   id: 10,
+  //   first_name: "Bryon",
+  //   last_name: "Zupo"
+  // },
+  // {
+  //   id: 11,
+  //   first_name: "Junette",
+  //   last_name: "Titheridge"
+  // },
+  // {
+  //   id: 12,
+  //   first_name: "Merrie",
+  //   last_name: "Pusey"
+  // },
+  // {
+  //   id: 13,
+  //   first_name: "Amelia",
+  //   last_name: "Stother"
+  // },
+  // {
+  //   id: 14,
+  //   first_name: "Rollo",
+  //   last_name: "Gamblin"
+  // },
+  // {
+  //   id: 15,
+  //   first_name: "Pavlov",
+  //   last_name: "Blunsom"
+  // },
+  // {
+  //   id: 16,
+  //   first_name: "Raddie",
+  //   last_name: "Cockerill"
+  // },
+  // {
+  //   id: 17,
+  //   first_name: "Bobbie",
+  //   last_name: "Randales"
+  // },
+  // {
+  //   id: 18,
+  //   first_name: "Dorette",
+  //   last_name: "Duffet"
+  // },
+  // {
+  //   id: 19,
+  //   first_name: "Tanitansy",
+  //   last_name: "A'field"
+  // },
+  // {
+  //   id: 20,
+  //   first_name: "Elvera",
+  //   last_name: "Anthoin"
+  // },
+  // {
+  //   id: 21,
+  //   first_name: "Ginger",
+  //   last_name: "Kopf"
+  // },
+  // {
+  //   id: 22,
+  //   first_name: "Aila",
+  //   last_name: "Killcross"
+  // },
+  // {
+  //   id: 23,
+  //   first_name: "Mycah",
+  //   last_name: "Paris"
+  // },
+  // {
+  //   id: 24,
+  //   first_name: "Patti",
+  //   last_name: "Quickenden"
+  // },
+  // {
+  //   id: 25,
+  //   first_name: "Cirillo",
+  //   last_name: "Rollings"
+  // },
+  // {
+  //   id: 26,
+  //   first_name: "Alameda",
+  //   last_name: "Kitcher"
+  // },
+  // {
+  //   id: 27,
+  //   first_name: "Mitzi",
+  //   last_name: "Pullin"
+  // },
+  // {
+  //   id: 28,
+  //   first_name: "Davide",
+  //   last_name: "Joust"
+  // },
+  // {
+  //   id: 29,
+  //   first_name: "Olenolin",
+  //   last_name: "Lean"
+  // },
+  // {
+  //   id: 30,
+  //   first_name: "Clarance",
+  //   last_name: "Clear"
+  // }
 ];
 
 const MessageRenderer = props => {
   return <p>{props.propsMessage}</p>;
 };
 
-const Friend = props => <div> {props.name}</div>;
+const Friend = props => {
+  const name = props.name;
+  const id = props.id;
+  return (
+    <div>
+      <h4> ID: {id} </h4>
+      <p> {name}</p>
+    </div>
+  );
+};
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      message: "Hello world "
+      message: "Hello world ",
+      friends: friendData
     };
   }
 
+  handleUpdateState = () => {
+    const friends = this.state.friends.slice();
+    friends.push({
+      id: 31,
+      first_name: "David",
+      last_name: "Chua"
+    });
+    // push our new friend into the friends array
+    this.setState({ friends: friends });
+    // FROM REACT API the only way to change state;
+  };
+
   render() {
+    console.log("STATE: ", this.state);
     return (
       <div>
         {this.state.message !== "" ? (
           <MessageRenderer propsMessage={this.state.message} />
         ) : null}
-        {friendData.map(friend => (
-          <Friend name={friend.first_name + " " + friend.last_name} />
+        {this.state.friends.map(friend => (
+          <Friend
+            id={friend.id}
+            name={friend.first_name + " " + friend.last_name}
+          />
         ))}
+        <button onClick={this.handleUpdateState}>
+          {" "}
+          Click to Add a Friend{" "}
+        </button>
       </div>
     );
   }
